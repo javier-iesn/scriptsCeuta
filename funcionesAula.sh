2fa239fbd923f016c48c39dbaa57849a6d94e275
Javier <javier.iesn@gmail.com>
2017-09-18 19:33:20 +0200

    Actualización 2017-09-18:17:330

---
+++ funcionesAula.sh
@@ -0,0 +1,115 @@
+macsCiclo1() {
+echo "172.124.116.101    f4:6d:04:54:30:8d    ASUSTek COMPUTER INC.
+172.124.116.102    f4:6d:04:54:30:95    ASUSTek COMPUTER INC.
+172.124.116.103    f4:6d:04:54:30:e3    ASUSTek COMPUTER INC.
+172.124.116.104    f4:6d:04:54:31:b7    ASUSTek COMPUTER INC.
+172.124.116.105    f4:6d:04:54:30:d6    ASUSTek COMPUTER INC.
+172.124.116.106    f4:6d:04:54:30:ec    ASUSTek COMPUTER INC.
+172.124.116.107    f4:6d:04:54:30:d2    ASUSTek COMPUTER INC.
+172.124.116.108    f4:6d:04:54:30:e0    ASUSTek COMPUTER INC.
+172.124.116.109    f4:6d:04:54:30:e5    ASUSTek COMPUTER INC.
+172.124.116.110    f4:6d:04:54:30:d3    ASUSTek COMPUTER INC.
+172.124.116.111    f4:6d:04:54:31:1c    ASUSTek COMPUTER INC.
+172.124.116.112    f4:6d:04:54:30:f0    ASUSTek COMPUTER INC.
+172.124.116.113    f4:6d:04:54:30:db    ASUSTek COMPUTER INC.
+172.124.116.114    f4:6d:04:54:31:c1    ASUSTek COMPUTER INC.
+172.124.116.115    f4:6d:04:54:2d:99    ASUSTek COMPUTER INC.
+172.124.116.116    f4:6d:04:54:2d:93    ASUSTek COMPUTER INC.
+172.124.116.117    f4:6d:04:52:b3:3e    ASUSTek COMPUTER INC.
+172.124.116.118    f4:6d:04:4f:be:79    ASUSTek COMPUTER INC.
+172.124.116.119    f4:6d:04:54:31:1a    ASUSTek COMPUTER INC.
+172.124.116.120    f4:6d:04:54:30:c4    ASUSTek COMPUTER INC.
+172.124.116.121    f4:6d:04:54:30:fc    ASUSTek COMPUTER INC.
+172.124.116.122    f4:6d:04:54:30:91    ASUSTek COMPUTER INC.
+172.124.116.123    f4:6d:04:54:30:d5    ASUSTek COMPUTER INC.
+172.124.116.124    f4:6d:04:54:30:f2    ASUSTek COMPUTER INC.
+172.124.116.125    f4:6d:04:54:30:dc    ASUSTek COMPUTER INC.
+172.124.116.126    f4:6d:04:54:30:cf    ASUSTek COMPUTER INC.
+172.124.116.127    48:5b:39:f7:5f:33    ASUSTek COMPUTER INC.
+172.124.116.128    f4:6d:04:52:b5:a5    ASUSTek COMPUTER INC.
+172.124.116.129    f4:6d:04:54:31:44    ASUSTek COMPUTER INC.
+172.124.116.130    f4:6d:04:54:30:8c    ASUSTek COMPUTER INC."
+}
+macsCiclo2() {
+echo "172.124.117.1    1c:1b:0d:0d:2d:85    (Unknown)
+172.124.117.2     1c:1b:0d:64:fc:c7    (Unknown)
+172.124.117.3     1c:1b:0d:64:fc:bf    (Unknown)
+172.124.117.40    1c:1b:0d:0c:68:d8    (Unknown)
+172.124.117.5     1c:1b:0d:0c:68:d9    (Unknown)
+172.124.117.6     1c:1b:0d:64:fc:b9    (Unknown)
+172.124.117.7     1c:1b:0d:65:88:61    (Unknown)
+172.124.117.8     1c:1b:0d:65:88:3b    (Unknown)
+172.124.117.9     1c:1b:0d:64:fc:bd    (Unknown)
+172.124.117.10    1c:1b:0d:65:88:39    (Unknown)
+172.124.117.11    1c:1b:0d:0c:69:24    (Unknown)
+172.124.117.12    1c:1b:0d:0d:3f:cc    (Unknown)
+172.124.117.13    1c:1b:0d:64:fc:e3    (Unknown)
+172.124.117.14    1c:1b:0d:65:88:5b    (Unknown)
+172.124.117.15    1c:1b:0d:0d:2d:87    (Unknown)
+172.124.117.16    1c:1b:0d:65:88:46    (Unknown)
+172.124.117.17    1c:1b:0d:65:88:8e    (Unknown)
+172.124.117.18    1c:1b:0d:65:88:a0    (Unknown)
+172.124.117.19    1c:1b:0d:65:88:9e    (Unknown)
+172.124.117.20    1c:1b:0d:0d:3f:ba    (Unknown)
+172.124.117.21    1c:1b:0d:65:88:9b    (Unknown)"
+}
+
+clavesCiclo1() {
+RED=172.124.116
+echo "${RED}.101    29583    74602
+${RED}.102    59394    68201
+${RED}.103    73309    62798
+${RED}.104    87903    44301
+${RED}.105    47689    26073
+${RED}.106    39005    61242
+${RED}.107    53509    28862
+${RED}.108    16101    21573
+${RED}.109    14589    37702
+${RED}.110    64063    29503
+${RED}.111    22410    83997
+${RED}.112    27410    95036
+${RED}.113    68440    33905
+${RED}.114    57033    24795
+${RED}.115    65991    39813
+${RED}.116    82511    64274
+${RED}.117    59003    28101
+${RED}.118    63704    50909
+${RED}.119    37411    83904
+${RED}.120    25441    40309
+${RED}.121    54369    84030
+${RED}.122    62584    43799
+${RED}.123    84030    70499
+${RED}.124    70708    94309
+${RED}.125    64459    37084
+${RED}.126    84905    62848
+${RED}.127    52905    38773
+${RED}.128    27074    64208
+${RED}.129    65670    48709
+${RED}.130    69098    57043"
+}
+
+clavesCiclo2() {
+RED=172.124.117
+echo "${RED}.1    2145    8124
+${RED}.2     3132    9856
+${RED}.3     8714    1901
+${RED}.40    3718    7323
+${RED}.5     8137    2419
+${RED}.6     1816    8412
+${RED}.7     2434    1091
+${RED}.8     5158    1750
+${RED}.9     3978    4621
+${RED}.10    2173    3724
+${RED}.11    7913    5832
+${RED}.12    7129    9427
+${RED}.13    3721    2425
+${RED}.14    3191    1696
+${RED}.15    3115    4820
+${RED}.16    8456    7462
+${RED}.17    2245    8543
+${RED}.18    1287    3578
+${RED}.19    4856    1469
+${RED}.20    1145    8523
+${RED}.21    3687    9731"
+}
+
