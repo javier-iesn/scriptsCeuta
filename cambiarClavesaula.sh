80b24d515767673fa2aba03b439e625f08b41113
Javier Criado <javier.iesn@gmail.com>
2017-09-18 13:10:17 +0200

    Actualización 2017-09-18:11:100

--- cambiarClavesaula.sh
+++ cambiarClavesaula.sh
@@ -67,14 +67,23 @@ ${RED}.21	3687	9731"
 [ "$1" = "" ] && uso
 ([ "$1" != "ciclo1" ] && [ "$1" != "ciclo2" ]) && uso
 CICLO="$1"
-
-([ "$CICLO" = "ciclo1" ] && clavesCiclo1 || clavesCiclo2) | while read L; do
+CONT=1
+TEMP=$(tempfile)
+([ "$CICLO" = "ciclo1" ] && clavesCiclo1 || clavesCiclo2) |
+while read L; do
    PC=$(echo $L | awk '{print $1;}')
    CLAVE1=$(echo $L | awk '{print $2;}')
    CLAVE2=$(echo $L | awk '{print $3;}')
    eCLAVE1=$(openssl passwd $CLAVE1)
    eCLAVE2=$(openssl passwd $CLAVE2)
-   ssh $PC "sudo usermod -p $eCLAVE1 ciclomedio" &
-   ssh $PC "sudo usermod -p $eCLAVE2 ciclosuperior" &
+   echo "PC: $PC" 
+   ssh -n -q $PC "sudo usermod -p $eCLAVE1 ciclomedio"  &> $TEMP
+   [ "$(cat $TEMP | grep -i 'no existe')" != "" ] && echo "PC: $PC no existe el usuario ciclomedio" 
+   rm $TEMP
+   ssh -n -q $PC "sudo usermod -p $eCLAVE2 ciclosuperior" &> $TEMP
+   [ "$(cat $TEMP | grep -i 'no existe')" != "" ] && echo "PC: $PC no existe el usuario ciclosuperior" 
+   rm $TEMP
+   #echo "CONT: $CONT. L: $L" 
+   CONT=$(($CONT + 1))
    ##echo "PC: $PC. CLAVE1: $CLAVE1. CLAVE2: $CLAVE2"
-done
+done 
